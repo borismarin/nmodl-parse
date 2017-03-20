d5505fbeea526ea307e85e8962607babb9d35ad9
borismarin <borismarin@gmail.com>
2017-03-20 17:42:38 -0300

    added assign to full script test

--- nmodl/program.py
+++ nmodl/program.py
@@ -1,7 +1,9 @@
 import pyparsing as pp
 
-from nmodl import title, units, parameter, comment
+from nmodl import title, units, parameter, comment, assigned
 
 program = (pp.Optional(title.title)('title') &
            pp.Optional(units.units_blk)('units') &
-           pp.Optional(parameter.par_blk)('parameter')).ignore(comment.comments)
+           pp.Optional(parameter.par_blk)('parameter') &
+           pp.Optional(assigned.assigned_blk)('assigned')
+           ).ignore(comment.comments)
--- nmodl/tests/test_program.py
+++ nmodl/tests/test_program.py
@@ -2,15 +2,30 @@ from nmodl.program import program
 
 print(program.parseString('''
 TITLE test mod file
+
 UNITS{
     (mV) = (millivolt)
 }
+
+
 COMMENT
   time flies
     \t like an arrow
   fruits flies
 \t like a banana
 ENDCOMMENT
+
+ASSIGNED {
+        v               (mV)
+        celsius         (degC)
+        ina             (mA/cm2)
+        gna             (pS/um2)
+        ena             (mV)
+        minf            hinf
+        mtau (ms)       htau (ms)
+        tadj
+}
+
 PARAMETER{
     erev = -70  (mV) : comment till eol
     gnabar = .12 (mho/cm2) : one more
