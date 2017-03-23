a461b09871b5d0a0e34784ef6e2a5cd539e858c7
borismarin <borismarin@gmail.com>
2017-03-23 02:55:19 -0300

    parsing expresssions (funct,proc) + tests

---
+++ nmodl/tests/test_expression.py
@@ -0,0 +1,35 @@
+from textwrap import dedent
+
+import nmodl.expressions as ne
+
+
+def test_stmt():
+    assert(ne.expr.parseString('a = 10').asList() ==
+           [['a', '=', '10']])
+
+
+def test_if():
+    assert(ne.ifstmt.parseString('if(1+1 ==2) 1\n else sin(0e-1)').asList() ==
+           ['if', [['1', '+', '1'], '==', '2'], ['1'], 'else', ['sin', ['0e-1']]])
+
+
+def test_body():
+    stmts = dedent('''
+            a = 1
+            a + 1
+            if(a == 12){
+            1 + 1
+            2 % 21}
+            else
+            sin(x)
+            ''')
+    assert(ne.body.parseString(stmts).asList() ==
+           [[['a', '=', '1']],
+            [['a', '+', '1']],
+            ['if',
+              ['a', '==', '12'],
+              [[['1', '+', '1']], [['2', '%', '21']]],
+              'else',
+              ['sin', ['x']]]])
+
+
