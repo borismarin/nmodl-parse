22fe2ce21c759864f3cd2de9dda7da1479dfeeb2
borismarin <borismarin@gmail.com>
2017-03-25 00:48:34 -0300

    test for logical exprs

--- nmodl/tests/test_expression.py
+++ nmodl/tests/test_expression.py
@@ -1,5 +1,3 @@
-from textwrap import dedent
-
 import nmodl.expressions as ne
 
 
@@ -11,15 +9,21 @@ def test_if():
     assert(ne.Statement.parseString('if(1+1==2)x=1\n else x=sin(0e-1)')
            .asList() ==
            ['if', '1', '+', '1', '==', '2',
-            'x', '1', 
+            'x', '1',
             'else', 'x', 'sin', '0e-1'])
 
 
 def test_if2():
     assert(ne.IfStatement.parseString('if(abs(-1)==1)tan(pi/4)\n else log(-1)')
            .asList() ==
-           ['if', 
+           ['if',
             'abs', '-1', '==', '1', 'tan', 'pi', '/', '4',
             'else', 'log', '-1'])
+
+
+def test_logical():
+    assert(ne.LogExp.parseString('x>1 && x<1.0e0')
+           .asList() ==
+           ['x', '>', '1', '&&', 'x', '<', '1.0e0'])
 
 
