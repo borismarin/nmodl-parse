b8f8c797a0dda02ba4d71c74e40676d800b853bd
borismarin <borismarin@gmail.com>
2017-03-27 02:30:54 -0300

    removed TABLE related stuff from tests

--- nmodl/tests/test_program.py
+++ nmodl/tests/test_program.py
@@ -46,10 +46,6 @@ BREAKPOINT {
 
 
 PROCEDURE trates(v (mV)) {
-    TABLE minf,  hinf, mtau, htau
-    DEPEND celsius, temp, Ra, Rb, Rd, Rg, tha, thi1, thi2, qa, qi, qinf
-    FROM vmin TO vmax WITH 199
-
         rates(v): not consistently executed from here if usetable == 1
 
 :        tinc = -dt * tadj
