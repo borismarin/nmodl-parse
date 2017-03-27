968cb8d6c532a2a9a0b8a5725d4b51300225e9c0
borismarin <borismarin@gmail.com>
2017-03-27 00:54:14 -0300

    initial blk parsing in full test

--- nmodl/program.py
+++ nmodl/program.py
@@ -1,7 +1,7 @@
 import pyparsing as pp
 
 from nmodl import (assigned, breakpoint, title, parameter, units, procedure,
-                   function, state, comment, terminals)
+                   function, state, comment, terminals, initial)
 
 program = ((pp.Optional(title.title)('title') &
             pp.Optional(units.units_blk)('units') &
@@ -10,7 +10,8 @@ program = ((pp.Optional(title.title)('title') &
             pp.Optional(procedure.procedure_blk)('procedure') &
             pp.Optional(function.function_blk)('function') &
             pp.Optional(breakpoint.breakpoint_blk)('breakpoint') &
-            pp.Optional(state.state_blk)('state')
+            pp.Optional(state.state_blk)('state') &
+            pp.Optional(initial.initial_blk)('initial')
             )
            .ignore(comment.comments)
            .ignore(terminals.UNITSOFF)
