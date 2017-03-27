208a98819cfc28095be6e70678005a907cb53527
borismarin <borismarin@gmail.com>
2017-03-27 02:29:28 -0300

    crazier INIT test

--- nmodl/tests/test_initial.py
+++ nmodl/tests/test_initial.py
@@ -1,18 +1,20 @@
 from textwrap import dedent
 from nmodl.initial import initial_blk
-
+from nmodl.comment import comments
 
 def test_initial():
     fun = dedent('''
     INITIAL {
+        tadj = q10^((celsius - temp)/(10 (degC))) : make all threads calculate tadj at initialization
 
         trates(v+vshift)
         m = minf
         h = hinf
     }
     ''')
-    assert(initial_blk.parseString(fun).asList() ==
+    assert(initial_blk.ignore(comments).parseString(fun).asList() ==
            ['INITIAL',
+            ['tadj', '=', ['q10', '^', [['celsius', '-', 'temp'], '/', '10', 'degC']]],
             'trates',
             [['v', '+', 'vshift']],
             ['m', '=', 'minf'],
