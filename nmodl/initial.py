7df20f487f2f00a70f2b073bc74edc53a30edcbc
borismarin <borismarin@gmail.com>
2017-03-27 00:31:11 -0300

    added INITIAL block + tests

---
+++ nmodl/initial.py
@@ -0,0 +1,4 @@
+from nmodl.terminals import INITIAL
+from nmodl.expressions import func_body
+
+initial_blk = INITIAL + func_body
---
+++ nmodl/tests/test_initial.py
@@ -0,0 +1,17 @@
+from textwrap import dedent
+from nmodl.initial import initial_blk
+
+
+def test_initial():
+    fun = dedent('''
+    INITIAL {
+
+        trates(v+vshift)
+        m = minf
+        h = hinf
+    }
+    ''')
+    assert(initial_blk.parseString(fun).asList() ==
+           ['INITIAL', 'trates', 'v', '+', 'vshift',
+            'm', 'minf',
+            'h', 'hinf'])
