68849bbdc9010cf2bcd89d1ea1f4af0019f75647
borismarin <borismarin@gmail.com>
2017-03-31 01:07:14 -0300

    testing attribute directly

--- nmodl/tests/test_title.py
+++ nmodl/tests/test_title.py
@@ -12,3 +12,9 @@ def test_multi():
     assert(title.parseString(test_string)[0].parsed.asList() == 
            ['TITLE', 'time flies like an arrow'])
 
+
+def test_obj():
+    test_string = "TITLE time flies like an arrow"
+    assert(title.parseString(test_string)[0].title == 
+           'time flies like an arrow')
+
