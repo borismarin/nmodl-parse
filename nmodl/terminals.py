f2b2877f0590d431cddcc615f545ca8d5820551a
borismarin <borismarin@gmail.com>
2017-04-08 12:06:41 -0300

    had forgotten to save changes...

--- nmodl/terminals.py
+++ nmodl/terminals.py
@@ -29,7 +29,7 @@ UNITSON = pp.Keyword('UNITSON')
 UNITSOFF = pp.Keyword('UNITSOFF')
 THREADSAFE = pp.Keyword('THREADSAFE')
 
-FLOAT = pp.Regex('[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?')('float_literal')
+FLOAT = pp.Regex('[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?')('value')
 INT = pp.Word(pp.nums)
 
 ID = pp.Word(pp.alphas, pp.alphanums+'_')('id')  # TODO: allowed ids?
