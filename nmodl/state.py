f1a023b78ddf95346713a24655adb2a9998ecd60
borismarin <borismarin@gmail.com>
2017-04-14 15:50:07 -0300

    adding units to state vars

--- nmodl/state.py
+++ nmodl/state.py
@@ -1,4 +1,7 @@
 import pyparsing as pp
 from nmodl.terminals import STATE, LBRACE, RBRACE, ID
+from units import unit_ref
 
-state_blk = STATE - LBRACE + pp.Group(pp.OneOrMore(ID))('state_vars') + RBRACE
+state_var = ID('id') + pp.Optional(unit_ref)
+state_blk = STATE - LBRACE + pp.ZeroOrMore(
+    state_var('state_vars*')) + RBRACE
--- nmodl/tests/test_state.py
+++ nmodl/tests/test_state.py
@@ -4,13 +4,16 @@ from nmodl.state import state_blk
 def test_state():
 
     assert(state_blk.parseString("STATE{abc}").asList() ==
-           ['STATE', ['abc']]) 
+           ['STATE', 'abc'])
 
-    from textwrap import dedent
-    test_string = dedent("""
-    STATE {x y 
-    
-    wz}
-    """)
+    test_string = 'STATE {x y \n\n\t wz\n}'
     assert(state_blk.parseString(test_string).asList() ==
-           ['STATE', ['x', 'y', 'wz']])
+           ['STATE', 'x', 'y', 'wz'])
+
+
+def test_state_units():
+    sv = state_blk.parseString("STATE{a (mV) b c (mM)}").state_vars
+    assert(sv[0].id == 'a')
+    assert(sv[0].unit == 'mV')
+    assert(sv[1].id == 'b')
+    assert(sv[2].asList() == ['c', 'mM'])
